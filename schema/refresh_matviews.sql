d2bc93c80919ece04d16779780b3c1a8f228b0b2
François Lacombe <francois.lacombe@infos-reseaux.com>
2025-10-26 16:24:20 +0100

    Add pdm ftth views to refresh and uninstall

--- schema/refresh_matviews.sql
+++ schema/refresh_matviews.sql
@@ -5,7 +5,11 @@ REFRESH MATERIALIZED VIEW CONCURRENTLY power_substation_relation;
 /* gespot */
 UPDATE osm_power_line SET tags=(tags::hstore - 'circuits'::text) where tags->'circuits' !~ '^ *[-+]?[0-9]*([.][0-9]+)?[0-9]*(([eE][-+]?)[0-9]+)? *$';
 
-/* Projects enedis */
+/* PDM enedis */
 REFRESH MATERIALIZED VIEW pdm_boundary;
 REFRESH MATERIALIZED VIEW pdm_project_poteaux;
 REFRESH MATERIALIZED VIEW pdm_project_substations;
+
+/* PDM FTTH */
+REFRESH MATERIALIZED VIEW pdm_project_connpoints;
+REFRESH MATERIALIZED VIEW pdm_project_exchanges;
--- schema/views_clean.sql
+++ schema/views_clean.sql
@@ -4,9 +4,13 @@ DROP MATERIALIZED VIEW IF EXISTS power_substation_relation;
 DROP VIEW IF EXISTS power_plant;
 DROP MATERIALIZED VIEW IF EXISTS power_plant_relation;
 
-/* Projects enedis */
+/* PDM enedis */
 DROP MATERIALIZED VIEW pdm_boundary CASCADE;
 DROP MATERIALIZED VIEW pdm_project_poteaux;
 DROP VIEW pdm_project_poteaux_other;
 DROP VIEW pdm_project_poteaux_noop;
 DROP MATERIALIZED VIEW pdm_project_substations;
+
+/* PDM FTTH */
+DROP MATERIALIZED VIEW pdm_project_connpoints;
+DROP MATERIALIZED VIEW pdm_project_exchanges;
